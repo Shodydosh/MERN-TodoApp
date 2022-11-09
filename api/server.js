3c5e13fd8b3144ea05e55742d66cc2e1ee956cdf
Tung Nguyen Minh <87895460+Shodydosh@users.noreply.github.com>
2022-11-10 00:11:26 +0700

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,3 +1,66 @@
+const app = require("./app");
+const debug = require("debug")("todo-api");
+const http = require("http");
+
+// Normalize Port
+const normalizePort = (val) => {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+};
+
+// Setting up callbacks
+// incase errors are encountered
+const onError = (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(`${bind} requires elevated privileges`.bgRed);
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(`${bind} is already in use`.bgRed);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+  debug("Listening on " + bind);
+};
+
+const PORT = app.get("port");
+const onServerStart = () => {
+  console.log(`TODO Api server is running on PORT: ${PORT}`.brightYellow.bold);
+};
+
+// Set the PORT
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+// Start the Server
+const server = http.createServer(app);
+server.on("error", onError);
+server.on("listening", onListening);
+server.listen(port, onServerStart);
+
 // const app = require("./app"
 // const express = require("express");
 // const colors = require("colors");
@@ -69,68 +132,3 @@
 //     res.status(500).json({ msg: err });
 //   }
 // });
-
-//! NEW server.js
-
-const app = require("./app");
-const debug = require("debug")("todo-api");
-const http = require("http");
-
-// Normalize Port
-const normalizePort = (val) => {
-  var port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-};
-
-// Setting up callbacks
-// incase errors are encountered
-const onError = (error) => {
-  if (error.syscall !== "listen") {
-    throw error;
-  }
-  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
-  switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges");
-      process.exit(1);
-      break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use");
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-};
-
-const onListening = () => {
-  const addr = server.address();
-  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
-  debug("Listening on " + bind);
-};
-
-const PORT = app.get("port");
-const onServerStart = () => {
-  console.log(`TODO Api server is running on PORT: ${PORT}`..brightYellow.bold);
-};
-
-// Set the PORT
-const port = normalizePort(process.env.PORT || "3000");
-app.set("port", port);
-
-// Start the Server
-const server = http.createServer(app);
-server.on("error", onError);
-server.on("listening", onListening);
-server.listen(port, onServerStart);
